bcfc453000189c525f2afa85bae12682caa43ad4
Alexandre Masson <alexandre.masson@virtuoworks.com>
2021-09-15 12:54:21 +0200

    Renamed scripts folder to bin and script to test.sh. Also modified the package.json accordingly

---
+++ bin/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# ******************************************** #
+# Create environment variables for credentials #
+#     based on arguments passed at runtime     #
+# ******************************************** #
+
+env CODEABONNE="$1" IDENTIFIANT="$2" MOTDEPASSE="$3" tsdx test
